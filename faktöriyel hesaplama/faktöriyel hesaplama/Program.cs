735e89677e5fab70425b518c66d79659b8db90ab
Keskinok <154586166+Keskinok@users.noreply.github.com>
2024-01-09 16:59:30 +0300

    Add files via upload

---
+++ faktöriyel hesaplama/faktöriyel hesaplama/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace faktöriyel_hesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                {
+                    int i = 1;
+                    Console.Write(" Faktöriyelini hesaplamak istediğiniz sayıyı gir: ");
+                    double sayi = Convert.ToInt32(Console.ReadLine());
+                    double faktoriyel = 1;
+                    while (i <= sayi)
+                    {
+                        faktoriyel = faktoriyel * i;
+                        i++;
+                    }
+                    Console.WriteLine(sayi + faktoriyel);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
+    
